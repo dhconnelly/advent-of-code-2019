b7973bbaf10a13270fb5587b18e8f7741de71daf
Daniel Connelly <dhconnelly@gmail.com>
2019-12-03 08:08:37 -0500

    cleanups for day 3

--- day3/day3.go
+++ day3/day3.go
@@ -9,135 +9,108 @@ import (
 	"strings"
 )
 
-type movement struct {
+type vec struct {
 	dir  rune
 	dist int
-}
-
-type path struct {
-	movs []movement
-}
-
-func readPaths(filePath string) []path {
-	txt, err := ioutil.ReadFile(filePath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	lines := strings.TrimSpace(string(txt))
-	var paths []path
-	for _, line := range strings.Split(lines, "\n") {
-		var p path
-		for _, tok := range strings.Split(line, ",") {
-			var mov movement
-			_, err := fmt.Sscanf(tok, "%c%d", &mov.dir, &mov.dist)
-			if err != nil {
-				log.Fatalf("failed to parse movement: %s", tok)
-			}
-			p.movs = append(p.movs, mov)
-		}
-		paths = append(paths, p)
-	}
-	return paths
 }
 
 type coord struct {
 	x, y int
 }
 
-func toCoords(p path) []coord {
-	var coords []coord
+func readVecPaths(filePath string) [][]vec {
+	txt, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	lines := strings.TrimSpace(string(txt))
+	var vecPaths [][]vec
+	for _, line := range strings.Split(lines, "\n") {
+		var vecPath []vec
+		for _, tok := range strings.Split(line, ",") {
+			var v vec
+			_, err := fmt.Sscanf(tok, "%c%d", &v.dir, &v.dist)
+			if err != nil {
+				log.Fatalf("failed to parse vec: %s", tok)
+			}
+			vecPath = append(vecPath, v)
+		}
+		vecPaths = append(vecPaths, vecPath)
+	}
+	return vecPaths
+}
+
+func toPath(vecPath []vec) []coord {
+	var path []coord
 	var cur coord
-	for _, mov := range p.movs {
+	for _, v := range vecPath {
 		var dim *int
-		d, n := 0, 0
-		switch mov.dir {
+		d := 0
+		switch v.dir {
 		case 'U':
 			dim = &cur.y
-			d, n = +1, mov.dist
+			d = +1
 		case 'D':
 			dim = &cur.y
-			d, n = -1, mov.dist
+			d = -1
 		case 'R':
 			dim = &cur.x
-			d, n = +1, mov.dist
+			d = +1
 		case 'L':
 			dim = &cur.x
-			d, n = -1, mov.dist
+			d = -1
 		default:
-			log.Fatalf("bad direction: %s", mov.dir)
+			log.Fatalf("bad direction: %s", v.dir)
 		}
-		for ; n > 0; n-- {
+		for n := v.dist; n > 0; n-- {
 			*dim += d
-			coords = append(coords, cur)
+			path = append(path, cur)
 		}
 	}
-	return coords
+	return path
 }
 
-type coordSet = []coord
-
-func allToCoords(paths []path) []coordSet {
-	var s []coordSet
-	for _, p := range paths {
-		s = append(s, toCoords(p))
+func findIntersects(coords1, coords2 []coord) []coord {
+	coords := make(map[coord]bool)
+	for _, c := range coords1 {
+		coords[c] = true
 	}
-	return s
-}
-
-func findIntersects(coordSets []coordSet) []coord {
-	var intersects []coord
-	// track coord -> which coordSets are occupying it
-	m := make(map[coord]map[int]bool)
-	for i, coordSet := range coordSets {
-		for _, coord := range coordSet {
-			if m[coord] == nil {
-				// not yet occupied
-				m[coord] = make(map[int]bool)
-				m[coord][i] = true
-				continue
-			}
-			m[coord][i] = true
-			if len(m[coord]) > 1 {
-				// if more than one set occupying: intersection
-				intersects = append(intersects, coord)
-			}
+	var intersections []coord
+	for _, c := range coords2 {
+		if coords[c] {
+			intersections = append(intersections, c)
 		}
 	}
-	return intersects
+	return intersections
 }
 
 func dist(c coord) int {
 	return int(math.Abs(float64(c.x)) + math.Abs(float64(c.y)))
 }
 
-func closestIntersect(intersects []coord) coord {
-	closest, closestDist := intersects[0], dist(intersects[0])
-	for _, coord := range intersects[1:] {
-		if d := dist(coord); d < closestDist {
-			closest, closestDist = coord, d
+func closestIntersect(intersects []coord) int {
+	var closestDist int
+	for _, coord := range intersects {
+		if d := dist(coord); closestDist == 0 || d < closestDist {
+			closestDist = d
 		}
 	}
-	return closest
+	return closestDist
 }
 
-func stepsToCoord(c coord, p path) int {
-	steps := 0
-	for _, pc := range toCoords(p) {
-		steps++
-		if pc == c {
-			break
+func stepsTo(to coord, path []coord) int {
+	for i, cur := range path {
+		if cur == to {
+			return i + 1
 		}
 	}
-	return steps
+	return 0
 }
 
-func fastestIntersect(intersects []coord, paths []path) int {
+func fastestIntersect(coords []coord, path1, path2 []coord) int {
 	var speed int
-	for _, c := range intersects {
-		sum := 0
-		for _, p := range paths {
-			sum += stepsToCoord(c, p)
-		}
+	for _, c := range coords {
+		sum := stepsTo(c, path1) + stepsTo(c, path2)
 		if speed == 0 || sum < speed {
 			speed = sum
 		}
@@ -146,9 +119,9 @@ func fastestIntersect(intersects []coord, paths []path) int {
 }
 
 func main() {
-	paths := readPaths(os.Args[1])
-	coords := allToCoords(paths)
-	intersects := findIntersects(coords)
-	fmt.Println(dist(closestIntersect(intersects)))
-	fmt.Println(fastestIntersect(intersects, paths))
+	vecPaths := readVecPaths(os.Args[1])
+	path1, path2 := toPath(vecPaths[0]), toPath(vecPaths[1])
+	intersects := findIntersects(path1, path2)
+	fmt.Println(closestIntersect(intersects))
+	fmt.Println(fastestIntersect(intersects, path1, path2))
 }

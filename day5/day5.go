780830601cf99946bf2130bd41ace06fe6299f7a
Daniel Connelly <dhconnelly@gmail.com>
2019-12-05 06:35:52 -0500

    day 5 part 2

--- day5/day5.go
+++ day5/day5.go
@@ -23,7 +23,7 @@ type instr struct {
 }
 
 var opcodeToParam = map[int]int{
-	1: 3, 2: 3, 3: 1, 4: 1, 99: 0,
+	1: 3, 2: 3, 3: 1, 4: 1, 5: 2, 6: 2, 7: 3, 8: 3, 99: 0,
 }
 
 func parseInstr(i int) instr {
@@ -36,6 +36,14 @@ func parseInstr(i int) instr {
 	return in
 }
 
+func atoi(a string) int {
+	i, err := strconv.Atoi(a)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return i
+}
+
 func read(path string) []int {
 	txt, err := ioutil.ReadFile(path)
 	if err != nil {
@@ -44,10 +52,7 @@ func read(path string) []int {
 	toks := strings.Split(strings.TrimSpace(string(txt)), ",")
 	data := make([]int, len(toks))
 	for i, tok := range toks {
-		data[i], err = strconv.Atoi(tok)
-		if err != nil {
-			log.Fatal(err)
-		}
+		data[i] = atoi(tok)
 	}
 	return data
 }
@@ -73,32 +78,85 @@ func run(data []int, in <-chan int, out chan<- int) {
 			r := get(data, i+2, instr.modes[1])
 			s := data[i+3]
 			data[s] = l + r
+			i += instr.params + 1
 		case 2:
 			l := get(data, i+1, instr.modes[0])
 			r := get(data, i+2, instr.modes[1])
 			s := data[i+3]
 			data[s] = l * r
+			i += instr.params + 1
 		case 3:
 			s := data[i+1]
 			data[s] = <-in
+			i += instr.params + 1
 		case 4:
 			v := get(data, i+1, instr.modes[0])
 			out <- v
+			i += instr.params + 1
+		case 5:
+			l := get(data, i+1, instr.modes[0])
+			r := get(data, i+2, instr.modes[1])
+			if l != 0 {
+				i = r
+			} else {
+				i += instr.params + 1
+			}
+		case 6:
+			l := get(data, i+1, instr.modes[0])
+			r := get(data, i+2, instr.modes[1])
+			if l == 0 {
+				i = r
+			} else {
+				i += instr.params + 1
+			}
+		case 7:
+			l := get(data, i+1, instr.modes[0])
+			r := get(data, i+2, instr.modes[1])
+			s := data[i+3]
+			if l < r {
+				data[s] = 1
+			} else {
+				data[s] = 0
+			}
+			i += instr.params + 1
+		case 8:
+			l := get(data, i+1, instr.modes[0])
+			r := get(data, i+2, instr.modes[1])
+			s := data[i+3]
+			if l == r {
+				data[s] = 1
+			} else {
+				data[s] = 0
+			}
+			i += instr.params + 1
 		case 99:
 			close(out)
-			break
+			return
 		}
-		i += instr.params + 1
 	}
+}
+
+func copied(data []int) []int {
+	data2 := make([]int, len(data))
+	copy(data2, data)
+	return data2
+}
+
+func execute(data []int, input int) int {
+	data = copied(data)
+	in, out := make(chan int, 1), make(chan int)
+	in <- input
+	go run(data, in, out)
+	var o int
+	for o = range out {
+	}
+	close(in)
+	return o
 }
 
 func main() {
 	data := read(os.Args[1])
-	in, out := make(chan int, 1), make(chan int)
-	in <- 1
-	go run(data, in, out)
-	for o := range out {
-		fmt.Println(o)
+	for _, a := range os.Args[2:] {
+		fmt.Println(execute(data, atoi(a)))
 	}
-	close(in)
 }

680c70f6fd29ef8f8c53784da56cba52550570c8
Daniel Connelly <dhconnelly@gmail.com>
2019-12-16 16:26:54 -0500

    day16: part2 success on the examples

--- day16/day16.go
+++ day16/day16.go
@@ -35,7 +35,14 @@ func coef(row, col int) int {
 	}
 }
 
+func copied(data []int) []int {
+	data2 := make([]int, len(data))
+	copy(data2, data)
+	return data2
+}
+
 func fft(signal []int, phases int) []int {
+	signal = copied(signal)
 	scratch := make([]int, len(signal))
 	for ; phases > 0; phases-- {
 		for i := 0; i < len(signal); i++ {
@@ -66,13 +73,60 @@ func offset(b []byte) int {
 	return i
 }
 
+func indexRepeated(signal []int, i int) int {
+	return signal[i%len(signal)]
+}
+
+func sliceSignal(signal []int, from, to int) []int {
+	sliced := make([]int, to-from)
+	for i := range sliced {
+		sliced[i] = indexRepeated(signal, from+i)
+	}
+	return sliced
+}
+
+func extractMessage(signal []int, reps, phases, offset, digits int) []int {
+	msg := sliceSignal(signal, offset, len(signal)*reps)
+	n := len(msg)
+
+	fmt.Println("findings coefs")
+	coefs := make([][]int, n)
+	for i := 0; i < n; i++ {
+		coefs[i] = make([]int, n-i)
+		for j := i; j < n; j++ {
+			coefs[i][j-i] = coef(offset+i+1, offset+j+1)
+		}
+	}
+
+	scratch := make([]int, n)
+	for ; phases > 0; phases-- {
+		fmt.Println("phase:", phases)
+		for i := 0; i < n; i++ {
+			sum := 0
+			for j := i; j < n; j++ {
+				sum += (coefs[i][j-i] * msg[j])
+			}
+			scratch[i] = ints.Abs(sum) % 10
+		}
+		msg, scratch = scratch, msg
+	}
+
+	return msg[:digits]
+}
+
 func main() {
 	txt, err := ioutil.ReadFile(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
 	}
 	v := parseVec(bytes.TrimSpace(txt))
+	i := offset(txt)
+
 	const digits = 8
-	fmt.Println(toSignal(fft(v, 100)[:digits]))
-	fmt.Println(offset(txt))
+	const phases = 100
+	const reps = 10000
+
+	//fmt.Println(toSignal(fft(v, phases)[:digits]))
+	fmt.Println("offset:", i)
+	fmt.Println(toSignal(extractMessage(v, reps, phases, i, digits)))
 }

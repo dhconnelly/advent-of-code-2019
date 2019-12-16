6be786797a904126705238925bfdfe1d1b0f6967
Daniel Connelly <dhconnelly@gmail.com>
2019-12-16 15:18:33 -0500

    day16: remove explicit matrix

--- day16/day16.go
+++ day16/day16.go
@@ -35,84 +35,17 @@ func coef(row, col int) int {
 	}
 }
 
-func coefCol(col, width int) []int {
-	v := make([]int, width)
-	for i := 0; i < width; i++ {
-		v[i] = coef(i+1, col)
-	}
-	return v
-}
-
-func coefColPow(col, width, n int) []int {
-	v := coefCol(col, width)
-	for n--; n > 0; n-- {
-		v2 := make([]int, len(v))
-		for i := 0; i < width; i++ {
-			sum := 0
-			for j := 0; j < width; j++ {
-				sum += coef(i+1, j+1) * v[j]
-			}
-			v2[i] = sum
-		}
-		v = v2
-	}
-	return v
-}
-
-func transpose(mat [][]int) {
-	for i := 0; i < len(mat); i++ {
-		for j := i + 1; j < len(mat); j++ {
-			mat[i][j], mat[j][i] = mat[j][i], mat[i][j]
-		}
-	}
-}
-
-func coefMatPow(width, n int) [][]int {
-	mat := make([][]int, width)
-	for j := 0; j < width; j++ {
-		mat[j] = coefColPow(j+1, width, n)
-	}
-	transpose(mat)
-	return mat
-}
-
-func coefMat(width int) [][]int {
-	mat := make([][]int, width)
-	for i := 0; i < width; i++ {
-		mat[i] = make([]int, width)
-		for j := 0; j < width; j++ {
-			mat[i][j] = coef(i+1, j+1)
-		}
-	}
-	return mat
-}
-
-func printMat(mat [][]int) {
-	for _, row := range mat {
-		for _, col := range row {
-			fmt.Printf("%3d", col)
-		}
-		fmt.Println()
-	}
-}
-
-func dot(x, y []int) int {
-	sum := 0
-	for i := 0; i < len(x); i++ {
-		sum += x[i] * y[i]
-	}
-	return ints.Abs(sum) % 10
-}
-
 func fft(signal []int, phases int) []int {
-	mat := coefMat(len(signal))
-	signal2 := make([]int, len(signal))
+	scratch := make([]int, len(signal))
 	for ; phases > 0; phases-- {
-		copy(signal2, signal)
 		for i := 0; i < len(signal); i++ {
-			signal2[i] = dot(mat[i], signal)
+			sum := 0
+			for j := 0; j < len(signal); j++ {
+				sum += coef(i+1, j+1) * signal[j]
+			}
+			scratch[i] = ints.Abs(sum) % 10
 		}
-		copy(signal, signal2)
+		signal, scratch = scratch, signal
 	}
 	return signal
 }

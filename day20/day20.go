5a87935a9843224fbb1f1bd22cda3dd1b76ce20a
Daniel Connelly <dhconnelly@gmail.com>
2019-12-20 13:12:27 -0500

    day20: find portals

--- day20/day20.go
+++ day20/day20.go
@@ -84,19 +84,109 @@ innerHi:
 	return
 }
 
-type label struct{}
+type label [2]rune
+
+func (lbl label) String() string {
+	return string(lbl[:])
+}
+
+func isLabel(g grid, p geom.Pt2) bool {
+	c := g.g[p]
+	return 'A' <= c && c <= 'Z'
+}
+
+func flip(lbl label) label {
+	return label([2]rune{lbl[1], lbl[0]})
+}
+
+func addLabel(
+	g grid,
+	from geom.Pt2,
+	dir geom.Direction,
+	reversed bool,
+	adjs map[label][]geom.Pt2,
+	lbls map[geom.Pt2]label,
+) {
+	p := from.Go(dir)
+	if !isLabel(g, p) {
+		return
+	}
+	a, b := g.g[p], g.g[p.Go(dir)]
+	lbl := label([2]rune{a, b})
+	if dir == geom.Up || dir == geom.Left {
+		lbl = flip(lbl)
+	}
+	if reversed {
+		lbl = flip(lbl)
+	}
+	adjs[lbl] = append(adjs[lbl], from)
+	lbls[p] = lbl
+}
+
+func findOuterLabels(
+	g grid,
+	r geom.Rect,
+	adjs map[label][]geom.Pt2,
+	lbls map[geom.Pt2]label,
+) {
+	for x := r.Lo.X; x <= r.Hi.X; x++ {
+		addLabel(g, geom.Pt2{x, r.Lo.Y}, geom.Up, false, adjs, lbls)
+	}
+	for x := r.Lo.X; x <= r.Hi.X; x++ {
+		addLabel(g, geom.Pt2{x, r.Hi.Y}, geom.Down, false, adjs, lbls)
+	}
+	for y := r.Lo.Y; y <= r.Hi.Y; y++ {
+		addLabel(g, geom.Pt2{r.Lo.X, y}, geom.Left, false, adjs, lbls)
+	}
+	for y := r.Lo.Y; y <= r.Hi.Y; y++ {
+		addLabel(g, geom.Pt2{r.Hi.X, y}, geom.Right, false, adjs, lbls)
+	}
+}
+
+func findInnerLabels(
+	g grid,
+	r geom.Rect,
+	adjs map[label][]geom.Pt2,
+	lbls map[geom.Pt2]label,
+) {
+	for x := r.Lo.X; x <= r.Hi.X; x++ {
+		addLabel(g, geom.Pt2{x, r.Lo.Y - 1}, geom.Up, true, adjs, lbls)
+	}
+	for x := r.Lo.X; x <= r.Hi.X; x++ {
+		addLabel(g, geom.Pt2{x, r.Hi.Y + 1}, geom.Down, true, adjs, lbls)
+	}
+	for y := r.Lo.Y; y <= r.Hi.Y; y++ {
+		addLabel(g, geom.Pt2{r.Lo.X - 1, y}, geom.Left, true, adjs, lbls)
+	}
+	for y := r.Lo.Y; y <= r.Hi.Y; y++ {
+		addLabel(g, geom.Pt2{r.Hi.X + 1, y}, geom.Right, true, adjs, lbls)
+	}
+}
+
+func findLabels(
+	g grid, outer, inner geom.Rect,
+) (map[label][]geom.Pt2, map[geom.Pt2]label) {
+	adjs := make(map[label][]geom.Pt2)
+	lbls := make(map[geom.Pt2]label)
+	findOuterLabels(g, outer, adjs, lbls)
+	findInnerLabels(g, inner, adjs, lbls)
+	return adjs, lbls
+}
 
 type maze struct {
-	g      grid
-	outer  geom.Rect
-	inner  geom.Rect
-	labels map[label][]geom.Pt2
+	g     grid
+	outer geom.Rect
+	inner geom.Rect
+	adjs  map[label][]geom.Pt2
+	lbls  map[geom.Pt2]label
 }
 
 func readMaze(g grid) maze {
 	outer, inner := mazeBounds(g)
-	fmt.Println(mazeBounds(g))
-	return maze{g, inner, outer, nil}
+	adjs, lbls := findLabels(g, outer, inner)
+	fmt.Println("adjs:", adjs)
+	fmt.Println("lbls:", lbls)
+	return maze{g, inner, outer, adjs, lbls}
 }
 
 func main() {

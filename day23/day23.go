a3417a7def3a1287e555ac12781f6ae209131315
Daniel Connelly <dhconnelly@gmail.com>
2019-12-23 21:37:48 -0500

    day23: working on it

--- day23/day23.go
+++ day23/day23.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
 
 	"github.com/dhconnelly/advent-of-code-2019/intcode"
 )
+
+const idleReceiveCount = 100
 
 type packet struct {
 	from int64
@@ -27,16 +30,58 @@ func head(q []packet) int64 {
 	return q[0].x
 }
 
-func NewMachine(prog []int64, addr int64) machine {
+type idleMap struct {
+	sync.RWMutex
+	count int64
+	idles map[int64]int64
+}
+
+func NewIdleMap(count int64) *idleMap {
+	m := make(map[int64]int64)
+	return &idleMap{idles: m, count: count}
+}
+
+func (im *idleMap) incr(addr int64) {
+	im.Lock()
+	defer im.Unlock()
+	im.idles[addr]++
+}
+
+func (im *idleMap) reset(addr int64) {
+	im.Lock()
+	defer im.Unlock()
+	im.idles[addr] = 0
+}
+
+func (im *idleMap) allIdle() bool {
+	im.RLock()
+	defer im.RUnlock()
+	for addr := int64(0); addr < im.count; addr++ {
+		if im.idles[addr] < idleReceiveCount {
+			return false
+		}
+	}
+	return true
+}
+
+func NewMachine(
+	prog []int64,
+	addr int64,
+	idles *idleMap,
+) machine {
 	out, in := make(chan packet), make(chan packet)
 	min := make(chan int64)
 	mout := intcode.RunProgram(prog, min)
 	min <- int64(addr)
 	var q []packet
+
 	go func() {
 		for {
 			select {
 			case to, ok := <-mout:
+				if idles != nil {
+					idles.reset(addr)
+				}
 				if !ok {
 					close(in)
 					close(out)
@@ -47,10 +92,19 @@ func NewMachine(prog []int64, addr int64) machine {
 				y := <-mout
 				p := packet{addr, to, x, y}
 				out <- p
+
 			case p := <-in:
 				q = append(q, p)
+				if idles != nil {
+					idles.reset(addr)
+				}
+
 			case min <- head(q):
-				if head(q) != -1 {
+				if head(q) == -1 {
+					if idles != nil {
+						idles.incr(addr)
+					}
+				} else {
 					p := q[0]
 					q = q[1:]
 					min <- p.y
@@ -73,13 +127,42 @@ func listen(
 	closed <- addr
 }
 
-func networkSwitch(ms map[int64]machine, nat bool) packet {
+type nat struct {
+	last packet
+	m    machine
+}
+
+func NewNat(addr int64, idles *idleMap) nat {
+	in := make(chan packet)
+	out := make(chan packet)
+	n := nat{m: machine{addr: addr, in: in, out: out}}
+	go func() {
+		for {
+			select {
+			case p := <-in:
+				fmt.Println(p)
+				n.last = p
+			default:
+				if idles.allIdle() {
+					fmt.Println("all idle, sending", n.last)
+					idles.reset(0)
+					out <- packet{from: addr, to: 0, x: n.last.x, y: n.last.y}
+				}
+			}
+		}
+	}()
+	return n
+}
+
+func networkSwitch(ms map[int64]machine, n *nat) packet {
 	out := make(chan packet)
 	closed := make(chan int64)
 	closedCount := 0
 	for addr, m := range ms {
 		go listen(addr, m, out, closed)
 	}
+	var lastNatPacket packet
+
 loop:
 	for {
 		select {
@@ -87,10 +170,17 @@ loop:
 			if !ok {
 				break loop
 			}
-			if !nat && p.to == 255 {
+			if n == nil && p.to == 255 {
 				return p
 			}
+			if n != nil && p.from == 255 && p.to == 0 {
+				if lastNatPacket.y == p.y {
+					return lastNatPacket
+				}
+				lastNatPacket = p
+			}
 			ms[p.to].in <- p
+
 		case <-closed:
 			closedCount++
 			if closedCount == len(ms) {
@@ -104,14 +194,29 @@ loop:
 	return packet{}
 }
 
+func machines(
+	data []int64,
+	count int64,
+	idles *idleMap,
+) map[int64]machine {
+	ms := make(map[int64]machine)
+	for addr := int64(0); addr < count; addr++ {
+		ms[addr] = NewMachine(data, addr, idles)
+	}
+	return ms
+}
+
 func main() {
 	data, err := intcode.ReadProgram(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
 	}
-	ms := make(map[int64]machine)
-	for addr := int64(0); addr < 50; addr++ {
-		ms[addr] = NewMachine(data, addr)
-	}
-	fmt.Println(networkSwitch(ms, false).y)
+	ms := machines(data, 50, nil)
+	fmt.Println(networkSwitch(ms, nil).y)
+
+	idles := NewIdleMap(50)
+	n := NewNat(255, idles)
+	ms = machines(data, 50, idles)
+	ms[255] = n.m
+	fmt.Println(networkSwitch(ms, &n).y)
 }

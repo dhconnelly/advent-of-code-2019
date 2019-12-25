15a5af2337353bcb32384f6850d5efc27f836fac
Daniel Connelly <dhconnelly@gmail.com>
2019-12-24 20:05:48 -0500

    day24: part 2

--- day24/day24.go
+++ day24/day24.go
@@ -129,9 +129,67 @@ type tile struct {
 	depth int
 }
 
+func printGrid(g grid, depth int) {
+	for _, row := range g.depth(depth) {
+		for _, alive := range row {
+			if alive {
+				fmt.Printf("%c", '#')
+			} else {
+				fmt.Printf("%c", '.')
+			}
+		}
+		fmt.Println()
+	}
+	fmt.Println()
+}
+
 type grid struct {
 	width, height int
 	g             map[tile]bool
+}
+
+func (g grid) countBugs() int {
+	bugs := 0
+	for _, alive := range g.g {
+		if alive {
+			bugs++
+		}
+	}
+	return bugs
+}
+
+func (g grid) depth(depth int) [][]bool {
+	m := make([][]bool, g.height)
+	for row := 0; row < g.height; row++ {
+		m[row] = make([]bool, g.width)
+		for col := 0; col < g.width; col++ {
+			m[row][col] = g.g[tile{p: geom.Pt2{col, row}, depth: depth}]
+		}
+	}
+	return m
+}
+
+func (g grid) next() {
+	diff := make(map[tile]bool)
+
+	// iterate twice so that we can extend the grid to include
+	// consideration of neighboring empty tiles
+	for tile, alive := range g.g {
+		if alive && g.adjacentBugs(tile) != 1 {
+			diff[tile] = false
+		}
+	}
+	for tile, alive := range g.g {
+		adj := g.adjacentBugs(tile)
+		if !alive && (adj == 1 || adj == 2) {
+			diff[tile] = true
+		}
+	}
+
+	// apply diffs
+	for tile, alive := range diff {
+		g.g[tile] = alive
+	}
 }
 
 func toGrid(l layout) grid {
@@ -146,6 +204,20 @@ func toGrid(l layout) grid {
 	return grid{width: l.width, height: l.height, g: g}
 }
 
+func (g grid) adjacentBugs(t tile) int {
+	bugs := 0
+	for _, nbr := range g.adjacent(t) {
+		alive, ok := g.g[nbr]
+		if !ok {
+			g.g[nbr] = false
+		}
+		if alive {
+			bugs++
+		}
+	}
+	return bugs
+}
+
 func (g grid) adjacent(t tile) []tile {
 	var adj []tile
 
@@ -155,11 +227,11 @@ func (g grid) adjacent(t tile) []tile {
 		adj = append(adj, tile{p: q, depth: t.depth})
 	} else if t.p.X == 0 {
 		q := geom.Pt2{1, 2}
-		adj = append(adj, tile{p: q, depth: t.depth + 1})
+		adj = append(adj, tile{p: q, depth: t.depth - 1})
 	} else if t.p.X == 3 && t.p.Y == 2 {
 		for y := 0; y < g.height; y++ {
 			q := geom.Pt2{g.width - 1, y}
-			adj = append(adj, tile{p: q, depth: t.depth - 1})
+			adj = append(adj, tile{p: q, depth: t.depth + 1})
 		}
 	}
 
@@ -169,11 +241,11 @@ func (g grid) adjacent(t tile) []tile {
 		adj = append(adj, tile{p: q, depth: t.depth})
 	} else if t.p.X == g.width-1 {
 		q := geom.Pt2{3, 2}
-		adj = append(adj, tile{p: q, depth: t.depth + 1})
+		adj = append(adj, tile{p: q, depth: t.depth - 1})
 	} else if t.p.X == 1 && t.p.Y == 2 {
 		for y := 0; y < g.height; y++ {
 			q := geom.Pt2{0, y}
-			adj = append(adj, tile{p: q, depth: t.depth - 1})
+			adj = append(adj, tile{p: q, depth: t.depth + 1})
 		}
 	}
 
@@ -183,11 +255,11 @@ func (g grid) adjacent(t tile) []tile {
 		adj = append(adj, tile{p: q, depth: t.depth})
 	} else if t.p.Y == 0 {
 		q := geom.Pt2{2, 1}
-		adj = append(adj, tile{p: q, depth: t.depth + 1})
+		adj = append(adj, tile{p: q, depth: t.depth - 1})
 	} else if t.p.X == 2 && t.p.Y == 3 {
 		for x := 0; x < g.width; x++ {
 			q := geom.Pt2{x, g.height - 1}
-			adj = append(adj, tile{p: q, depth: t.depth - 1})
+			adj = append(adj, tile{p: q, depth: t.depth + 1})
 		}
 	}
 
@@ -197,11 +269,11 @@ func (g grid) adjacent(t tile) []tile {
 		adj = append(adj, tile{p: q, depth: t.depth})
 	} else if t.p.Y == g.height-1 {
 		q := geom.Pt2{2, 3}
-		adj = append(adj, tile{p: q, depth: t.depth + 1})
+		adj = append(adj, tile{p: q, depth: t.depth - 1})
 	} else if t.p.X == 2 && t.p.Y == 1 {
 		for x := 0; x < g.width; x++ {
 			q := geom.Pt2{x, 0}
-			adj = append(adj, tile{p: q, depth: t.depth - 1})
+			adj = append(adj, tile{p: q, depth: t.depth + 1})
 		}
 	}
 
@@ -221,11 +293,8 @@ func main() {
 	fmt.Println(findRepeat(l))
 
 	g := toGrid(l)
-	fmt.Println(g.adjacent(tile{p: geom.Pt2{3, 3}, depth: 1}))
-	fmt.Println(g.adjacent(tile{p: geom.Pt2{1, 1}, depth: 0}))
-	fmt.Println(g.adjacent(tile{p: geom.Pt2{3, 0}, depth: 0}))
-	fmt.Println(g.adjacent(tile{p: geom.Pt2{4, 0}, depth: 0}))
-	fmt.Println(g.adjacent(tile{p: geom.Pt2{3, 2}, depth: 1}))
-	fmt.Println(g.adjacent(tile{p: geom.Pt2{3, 2}, depth: 0}))
-	//fmt.Println(countBugs(g, 200))
+	for i := 0; i < 200; i++ {
+		g.next()
+	}
+	fmt.Println(g.countBugs())
 }

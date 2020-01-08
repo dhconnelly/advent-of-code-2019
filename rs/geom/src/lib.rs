1e0d9ccb2f5f5f8da274a475618be9cbbfd1bbeb
Daniel Connelly <dhconnelly@gmail.com>
2020-01-08 09:56:30 -0500

    rust geom: add go and manhattan_neighbors to Point2

--- rs/geom/src/lib.rs
+++ rs/geom/src/lib.rs
@@ -1,3 +1,11 @@
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
 struct Point2 {
     x: i64,
     y: i64,
@@ -10,6 +18,26 @@ impl Point2 {
 
     fn manhattan_norm(&self) -> i64 {
         self.manhattan_dist(&Point2 { x: 0, y: 0 })
+    }
+
+    fn go(&self, dir: Direction) -> Point2 {
+        let mut q = *self;
+        match dir {
+            Direction::Up => q.y += 1,
+            Direction::Down => q.y -= 1,
+            Direction::Left => q.x -= 1,
+            Direction::Right => q.x += 1,
+        }
+        q
+    }
+
+    fn manhattan_neighbors(&self) -> [Point2; 4] {
+        [
+            self.go(Direction::Up),
+            self.go(Direction::Down),
+            self.go(Direction::Left),
+            self.go(Direction::Right),
+        ]
     }
 }
 
@@ -24,4 +52,22 @@ fn manhattan_dist() {
 fn manhattan_norm() {
     let p = Point2 { x: 4, y: -5 };
     assert_eq!(p.manhattan_norm(), 9);
+}
+
+#[test]
+fn go() {
+    let p = Point2 { x: 4, y: -5 };
+    assert_eq!(p.go(Direction::Up), Point2 { x: 4, y: -4 });
+    assert_eq!(p.go(Direction::Down), Point2 { x: 4, y: -6 });
+    assert_eq!(p.go(Direction::Left), Point2 { x: 3, y: -5 });
+    assert_eq!(p.go(Direction::Right), Point2 { x: 5, y: -5 });
+}
+
+#[test]
+fn manhattan_neighbors() {
+    let p = Point2 { x: 4, y: -5 };
+    let nbrs = p.manhattan_neighbors();
+    for q in nbrs.iter() {
+        assert_eq!(p.manhattan_dist(q), 1);
+    }
 }

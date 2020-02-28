44d936be1b394e5f6c83d420a0dd5315f5c299d2
Daniel Connelly <dhconnelly@gmail.com>
2020-02-28 14:52:46 -0500

    ocaml day9: make store return vm in prep for another Map.Make attempt

--- ocaml/day9/vm.ml
+++ ocaml/day9/vm.ml
@@ -70,26 +70,28 @@ let ld x md vm = match md with
   | Rel -> get (vm.rel+x) vm.data
 
 let store y x md vm = match md with
-  | Pos -> set y x vm.data
+  | Pos -> set y x vm.data; vm
   | Imm -> failwith "invalid store in immediate mode"
-  | Rel -> set y (vm.rel+x) vm.data
+  | Rel -> set y (vm.rel+x) vm.data; vm
+
+let incr n vm = {vm with pc=vm.pc+n}
 
 let rec step ({pc; data; state} as vm) =
   let {op; modes=m1,m2,m3} = get pc data |> decode in
   let (a,b,c) = (get (pc+1) data), (get (pc+2) data), (get (pc+3) data) in
   let (x,y) = (ld a m1 vm), (ld b m2 vm) in
-  if state = Input then (
-    store vm.input a m1 vm;
+  if state = Input then 
+    let vm = store vm.input a m1 vm in
     step {vm with pc=pc+2; state=Running}
-  ) else match op with
-    | Add -> store (x+y) c m3 vm; {vm with pc=pc+4}
-    | Mul -> store (x*y) c m3 vm; {vm with pc=pc+4}
+  else match op with
+    | Add -> store (x+y) c m3 vm |> incr 4
+    | Mul -> store (x*y) c m3 vm |> incr 4
     | Read -> {vm with state=Input}
     | Write -> {vm with pc=pc+2; output=x; state=Output}
     | JmpIf -> {vm with pc=if x<>0 then y else pc+3}
     | JmpNot -> {vm with pc=if x=0 then y else pc+3}
-    | Lt -> store (if x<y then 1 else 0) c m3 vm; {vm with pc=pc+4}
-    | Eq -> store (if x=y then 1 else 0) c m3 vm; {vm with pc=pc+4}
+    | Lt -> store (if x<y then 1 else 0) c m3 vm |> incr 4
+    | Eq -> store (if x=y then 1 else 0) c m3 vm |> incr 4
     | AdjRel -> {vm with pc=pc+2; rel=vm.rel+x}
     | Halt -> {vm with pc=pc+1; state=Halted}
 

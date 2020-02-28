e97b8fa06fb10c2af4e7118fa46629647163660c
Daniel Connelly <dhconnelly@gmail.com>
2020-02-28 10:30:22 -0500

    ocaml day5: finish module

--- ocaml/day5/vm.ml
+++ ocaml/day5/vm.ml
@@ -1,3 +1,5 @@
+open Printf
+
 type data = int array
 
 let get pos data = data.(pos)
@@ -12,3 +14,35 @@ let read ic =
   | None -> acc
   | Some instr -> read_acc (instr::acc)
   in read_acc [] |> List.rev |> Array.of_list
+
+type opcode = Add | Mul | Halt
+
+let decode = function
+  | 1 -> Add
+  | 2 -> Mul
+  | 99 -> Halt
+  | o -> failwith (sprintf "invalid opcode: %d" o)
+
+type state = Running | Halted
+
+type vm = {
+  pc: int;
+  data: data;
+  state: state;
+}
+
+let vm_new data = {pc=0; data; state=Running}
+let vm_data vm = vm.data
+let ld pos data = get (get pos data) data
+let store pos x data = set x (get pos data) data
+
+let step {pc; data} =
+  let (a, b, z) = (ld (pc+1) data), (ld (pc+2) data), (pc+3) in
+  match get pc data |> decode with
+  | Add -> {pc=pc+4; data=(store z (a + b) data); state=Running}
+  | Mul -> {pc=pc+4; data=(store z (a * b) data); state=Running}
+  | Halt -> {pc=pc+1; data; state=Halted}
+
+let rec run vm = match vm with
+  | {state=Halted} -> vm
+  | _ -> run (step vm)
--- ocaml/day5/vm.mli
+++ ocaml/day5/vm.mli
@@ -1,6 +1,10 @@
 type data
-
 val read : Scanf.Scanning.in_channel -> data
 val copy : data -> data
 val set  : int -> int -> data -> data
 val get  : int -> data -> int
+
+type vm
+val vm_new   : data -> vm
+val vm_data  : vm -> data
+val run      : vm -> vm

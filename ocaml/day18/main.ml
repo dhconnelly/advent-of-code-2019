6ca80d311976dfbab99a4712d82d7fde2a09e484
Daniel Connelly <dconnelly@google.com>
2020-03-03 16:14:40 +0000

    ocaml day18: bfs

--- ocaml/day18/main.ml
+++ ocaml/day18/main.ml
@@ -1,28 +1,11 @@
 open Printf
 open Pt2
 module PtMap = Map.Make(Pt2)
+module PtSet = Set.Make(Pt2)
+module CharMap = Map.Make(Char)
 
+(* tiles *)
 type tile = Wall | Entrance | Passage | Door of char | Key of char
-
-let key_of ch = Char.lowercase_ascii ch
-let door_of ch = Char.uppercase_ascii ch
-
-type maze = {grid: tile PtMap.t; rows: int; cols: int}
-
-let print_tile = function
-  | Wall -> '#'
-  | Entrance -> '@'
-  | Passage -> '.'
-  | Door ch -> ch
-  | Key ch -> ch
-
-let print m =
-  for row=0 to m.rows-1 do
-    for col=0 to m.cols-1 do
-      print_tile (PtMap.find (col, row) m.grid) |> print_char
-    done;
-    print_newline ()
-  done
 
 let parse_tile row col c =
   let tile = match c with
@@ -34,20 +17,82 @@ let parse_tile row col c =
   | c -> failwith (sprintf "bad tile: %c" c) in
   (col, row), tile
 
-let read_chars ic = input_line ic |> String.to_seq |> List.of_seq
-let parse_tiles row chars = List.mapi (parse_tile row) chars
-let add_point grid (pt, tile) = PtMap.add pt tile grid
+let print_tile = function
+  | Wall -> '#'
+  | Entrance -> '@'
+  | Passage -> '.'
+  | Door ch -> ch
+  | Key ch -> ch
 
-let read_maze ic =
-  let rec loop row m =
-    try
-      let pts = read_chars ic |> parse_tiles row in
-      let grid = List.fold_left add_point m.grid pts in
-      let m = {grid; rows=row+1; cols=List.length pts} in
-      loop (row+1) m
-    with End_of_file -> m in
-  loop 0 {grid=PtMap.empty; rows=0; cols=0}
+let is_passable = function
+  | Entrance | Passage -> true
+  | _ -> false
+let is_door_or_key = function
+  | Door _ | Key _ -> true
+  | _ -> false
+let char_of = function
+  | Door ch | Key ch -> ch
+  | _ -> failwith "not a char tile"
 
+(* grid *)
+type grid = tile PtMap.t
+let add_point g (pt, tile) = PtMap.add pt tile g
+
+let read_grid ic =
+  let rec loop row g = try
+    let chars = input_line ic |> String.to_seq |> List.of_seq in
+    let tiles = List.mapi (parse_tile row) chars in
+    let g = List.fold_left add_point g tiles in
+    loop (row+1) g with End_of_file -> g in
+  loop 0 PtMap.empty
+
+let print_grid g =
+  let max_col, max_row = PtMap.fold
+    (fun (c,r) v (mc,mr) -> max mc c, max mr r) g (0,0) in
+  for row=0 to max_row do
+    for col=0 to max_col do
+      print_tile (PtMap.find (col, row) g) |> print_char
+    done;
+    print_newline ()
+  done
+
+(* bfs *)
+type dist_map = int CharMap.t
+
+let print_dists d =
+  CharMap.iter (fun ch dist -> printf "%c -> %d\n" ch dist) d
+
+type bfs_node = {pt: Pt2.t; tile: tile; dist: int}
+
+let node_of pt dist g = {pt; tile=PtMap.find pt g; dist}
+
+let bfs_nbrs pt g v dist =
+  let nbrs = Pt2.nbrs pt |> List.filter (fun pt -> PtMap.mem pt g) in
+  let to_visit = List.filter (fun pt -> PtSet.mem pt v |> not) nbrs in
+  let nodes = List.map (fun pt -> node_of pt dist g) to_visit in
+  List.filter (fun {tile} -> tile <> Wall) nodes
+
+let mark_visited v {pt} = PtSet.add pt v
+
+let update_dists d {pt; tile; dist} =
+  if is_door_or_key tile then CharMap.add (char_of tile) dist d else d
+
+let bfs pt g : dist_map =
+  let q = Queue.create () in Queue.add (node_of pt 0 g) q;
+  let rec loop v d = match Queue.take_opt q with
+    | None -> d
+    | Some nd -> visit v d nd
+  and visit v d {pt; tile; dist} =
+    let nbrs = bfs_nbrs pt g v (dist+1) in
+    let v = List.fold_left mark_visited v nbrs in
+    let d = List.fold_left update_dists d nbrs in
+    List.iter (fun nd -> if is_passable nd.tile then Queue.add nd q) nbrs;
+    loop v d in
+  loop (PtSet.add pt PtSet.empty) CharMap.empty
+
+(* main *)
 let () =
-  let m = open_in Sys.argv.(1) |> read_maze in
-  print m
+  let g = open_in Sys.argv.(1) |> read_grid in
+  print_grid g;
+  print_dists (bfs (8,4) g)
+

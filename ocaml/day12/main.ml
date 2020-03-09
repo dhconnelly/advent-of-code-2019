f1206368ef8888245aa1ca29ae428822fa7851e9
Daniel Connelly <dconnelly@google.com>
2020-03-09 14:23:22 +0000

    ocaml day12: part 1

--- ocaml/day12/main.ml
+++ ocaml/day12/main.ml
@@ -27,7 +27,15 @@ let pairs (ps: planet list): (planet * planet) list =
   List.combine ps ps |> List.filter (fun (p, q) -> p != q)
 
 let apply_gravity (ps: planet list): planet list =
-  ps
+  let update_vel (gx, gy, gz) (x, y, z) (dx, dy, dz) = 
+    (if gx < x then dx-1 else if gx > x then dx+1 else dx),
+    (if gy < y then dy-1 else if gy > y then dy+1 else dy),
+    (if gz < z then dz-1 else if gz > z then dz+1 else dz) in
+  let apply {pos=pos1} {pos=pos2; vel} =
+    {pos=pos2; vel=update_vel pos1 pos2 vel} in
+  let apply1 ps p =
+    List.fold_left (fun acc q -> apply p q::acc) [] ps in
+  List.fold_left apply1 ps ps
 
 let apply_velocity (ps: planet list): planet list =
   let apply1 ({pos=(x, y, z); vel=(dx, dy, dz)} as p) =
@@ -40,6 +48,11 @@ let step (ps: planet list): planet list =
 let rec steps (n: int) (ps: planet list): planet list =
   if n = 0 then ps else step ps |> steps (n-1)
 
+let energy (ps: planet list): int =
+  let energy1 {pos=(x, y, z); vel=(dx, dy, dz)} =
+    Int.((abs x + abs y + abs z) * (abs dx + abs dy + abs dz)) in
+  List.map energy1 ps |> List.fold_left (+) 0
+
 let () =
   let ps = open_in Sys.argv.(1) |> scan_planets in
-  steps 1000 ps |> print_planets
+  steps 1000 ps |> energy |> Printf.printf "%d\n"

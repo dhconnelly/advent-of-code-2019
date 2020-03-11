b2022294787317c81b9985ef6f1588be893342ba
Daniel Connelly <dhconnelly@gmail.com>
2020-03-11 12:15:22 -0400

    ocaml day14: initial state

--- ocaml/day14/main.ml
+++ ocaml/day14/main.ml
@@ -8,10 +8,8 @@ let reaction_delim = Str.regexp_string " => "
 let quants_delim = Str.regexp_string ", "
 
 let read_reactions (ic: in_channel): reaction_map =
-  let parse_quant s =
-    Scanf.sscanf s "%d %s" (fun amt chem -> {amt; chem}) in
-  let parse_quants qs =
-    Str.split quants_delim qs |> List.map parse_quant in
+  let parse_quant s = Scanf.sscanf s "%d %s" (fun amt chem -> {amt; chem}) in
+  let parse_quants qs = Str.split quants_delim qs |> List.map parse_quant in
   let parse_reaction line = match Str.split reaction_delim line with
   | ins::out::[] -> {ins=parse_quants ins; out=parse_quant out}
   | _ -> failwith ("bad reaction: " ^ line) in
@@ -29,6 +27,16 @@ let print_reactions (rs: reaction_map) =
 
 type react_state = {need: chem_map; have: chem_map}
 
+let initial_state (rs: reaction_map): react_state =
+  {need=StrMap.empty |> StrMap.add "FUEL" 1; have=StrMap.empty}
+
+let print_state ({need; have}: react_state) =
+  let print_chems chem amt = Printf.printf "%s: %d\n" chem amt in
+  Printf.printf "Need:\n";
+  StrMap.iter print_chems need;
+  Printf.printf "Have:\n";
+  StrMap.iter print_chems have
+
 let rec reduce (rs: reaction_map) (state: react_state): react_state =
   state
 
@@ -36,4 +44,6 @@ let cost (rs: reaction_map): int =
   0
 
 let () =
-  Sys.argv.(1) |> open_in |> read_reactions |> print_reactions
+  let reactions = Sys.argv.(1) |> open_in |> read_reactions in
+  let state = initial_state reactions in
+  print_state state

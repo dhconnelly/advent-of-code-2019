f44731f69908b75dfb320d057402b13926442fd2
Daniel Connelly <dhconnelly@gmail.com>
2020-03-22 14:21:56 -0400

    rust day 20: part 2

--- rs/day20/src/main.rs
+++ rs/day20/src/main.rs
@@ -142,13 +142,15 @@ fn portals_along<T: IntoIterator<Item = Point2>>(
     })
 }
 
-fn all_portals<'a>(tiles: &'a Tiles) -> HashMap<Portal, Vec<&'a Point2>> {
-    let Bounds {
+fn all_portals<'a>(
+    tiles: &'a Tiles,
+    Bounds {
         outer_lo,
         outer_hi,
         inner_lo,
         inner_hi,
-    } = maze_bounds(tiles);
+    }: &Bounds,
+) -> HashMap<Portal, Vec<&'a Point2>> {
     let pt = Point2::new;
     let outer_top = (outer_lo.x..=outer_hi.x).map(|x| pt(x, outer_lo.y));
     let outer_bottom = (outer_lo.x..=outer_hi.x).map(|x| pt(x, outer_hi.y));
@@ -190,36 +192,60 @@ fn link_portals<'a>(
     }
 }
 
+type Graph<'a> = HashMap<&'a Point2, HashSet<&'a Point2>>;
+
 struct Maze<'a> {
     begin: &'a Point2,
     end: &'a Point2,
-    nbrs: HashMap<&'a Point2, HashSet<&'a Point2>>,
+    nbrs: Graph<'a>,
+    bounds: Bounds<'a>,
 }
 
 fn read_maze(tiles: &Tiles) -> Maze {
     let mut nbrs = all_manhattan_nbrs(tiles);
-    let portals = all_portals(tiles);
+    let bounds = maze_bounds(tiles);
+    let portals = all_portals(tiles, &bounds);
     link_portals(&mut nbrs, &portals);
     let begin = portals[&('A', 'A')][0];
     let end = portals[&('Z', 'Z')][0];
-    Maze { begin, end, nbrs }
+    Maze {
+        begin,
+        end,
+        nbrs,
+        bounds,
+    }
 }
 
-fn dist(
-    nbrs: &HashMap<&Point2, HashSet<&Point2>>,
-    src: &Point2,
-    dst: &Point2,
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
+struct RecPt2<'a> {
+    p: &'a Point2,
+    lvl: usize,
+}
+
+impl<'a> RecPt2<'a> {
+    fn new(p: &'a Point2, lvl: usize) -> RecPt2<'a> {
+        RecPt2 { p, lvl }
+    }
+}
+
+fn dist<'a>(
+    maze: &'a Maze,
+    nbrs: impl Fn(&RecPt2, &'a Maze) -> Vec<RecPt2<'a>>,
+    eq: impl Fn(&RecPt2, &RecPt2) -> bool,
 ) -> Option<usize> {
+    let src = RecPt2::new(maze.begin, 0);
+    let dst = RecPt2::new(maze.end, 0);
     let mut q = VecDeque::new();
     let mut v = HashSet::new();
     q.push_back((src, 0));
     v.insert(src);
     while let Some((p, d)) = q.pop_front() {
-        for nbr in nbrs.get(p).unwrap() {
-            if *nbr == dst {
+        let ns = nbrs(&p, &maze);
+        for nbr in ns {
+            if eq(&nbr, &dst) {
                 return Some(d + 1);
             }
-            if v.contains(nbr) {
+            if v.contains(&nbr) {
                 continue;
             }
             v.insert(nbr);
@@ -229,11 +255,73 @@ fn dist(
     None
 }
 
+fn flat_eq(p: &RecPt2, q: &RecPt2) -> bool {
+    p.p == q.p
+}
+
+fn flat_nbrs<'a>(p: &RecPt2, maze: &'a Maze) -> Vec<RecPt2<'a>> {
+    maze.nbrs
+        .get(p.p)
+        .unwrap()
+        .iter()
+        .map(|p| RecPt2 { p, lvl: 0 })
+        .collect()
+}
+
+fn flat_dist(maze: &Maze) -> Option<usize> {
+    dist(&maze, flat_nbrs, flat_eq)
+}
+
+fn rec_eq(p: &RecPt2, q: &RecPt2) -> bool {
+    p == q
+}
+
+fn is_outer_edge(p: &RecPt2, maze: &Maze) -> bool {
+    p.p.x == maze.bounds.outer_lo.x
+        || p.p.x == maze.bounds.outer_hi.x
+        || p.p.y == maze.bounds.outer_lo.y
+        || p.p.y == maze.bounds.outer_hi.y
+}
+
+fn is_inner_edge(p: &RecPt2, maze: &Maze) -> bool {
+    p.p.x + 1 == maze.bounds.inner_lo.x
+        || p.p.x - 1 == maze.bounds.inner_hi.x
+        || p.p.y + 1 == maze.bounds.inner_lo.y
+        || p.p.y - 1 == maze.bounds.inner_hi.y
+}
+
+fn rec_nbrs<'a>(p: &RecPt2, maze: &'a Maze) -> Vec<RecPt2<'a>> {
+    let mut nbrs = Vec::new();
+    for nbr in maze.nbrs.get(p.p).unwrap() {
+        if nbr.manhattan_dist(p.p) == 1 {
+            // non-portal neighbors are always accessible.
+            nbrs.push(RecPt2::new(nbr, p.lvl));
+        } else if is_inner_edge(p, maze) {
+            // portal neighbors from the inner edge are always accessible,
+            // but at a greater recursion depth.
+            nbrs.push(RecPt2::new(nbr, p.lvl + 1));
+        } else if is_outer_edge(p, maze) && p.lvl > 0 {
+            // portal neighbors from the outer edge are only accessible from
+            // non-zero recursion depth. traversing them goes up a level.
+            nbrs.push(RecPt2::new(nbr, p.lvl - 1));
+        } else {
+            // portal neighbors from the outer edge at level zero are closed.
+            assert!(is_outer_edge(p, maze) && p.lvl == 0);
+        }
+    }
+    nbrs
+}
+
+fn rec_dist(maze: &Maze) -> Option<usize> {
+    dist(&maze, rec_nbrs, rec_eq)
+}
+
 fn main() -> Result<(), Box<dyn error::Error>> {
     let path = env::args().nth(1).ok_or("Usage: day20 <filename>")?;
     let text = fs::read_to_string(&path)?;
     let tiles = read_tiles(&text)?;
     let maze = read_maze(&tiles);
-    println!("{}", dist(&maze.nbrs, &maze.begin, &maze.end).unwrap());
+    println!("{}", flat_dist(&maze).unwrap());
+    println!("{}", rec_dist(&maze).unwrap());
     Ok(())
 }

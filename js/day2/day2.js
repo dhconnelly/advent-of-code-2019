76fdae2be64bfbf65d7c38448211a5de452c4ad5
Daniel Connelly <dhconnelly@gmail.com>
2021-04-06 21:51:38 -0400

    js day2 part2

--- js/day2/day2.js
+++ js/day2/day2.js
@@ -44,12 +44,23 @@ function run(prog, a, b) {
     return vm.mem[0];
 }
 
+function find(prog, target) {
+    for (let noun = 0; noun <= 99; noun++) {
+        for (let verb = 0; verb <= 99; verb++) {
+            if (run(prog, noun, verb) === target) {
+                return 100 * noun + verb;
+            }
+        }
+    }
+}
+
 function main(argv) {
     const path = argv[0];
     const file = fs.readFileSync(path, "ascii");
     const toks = file.split(",");
     const prog = toks.map((s) => parseInt(s, 10));
     console.log(run(prog, 12, 2));
+    console.log(find(prog, 19690720));
 }
 
 main(process.argv.slice(2));

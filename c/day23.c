cfde51563624caa63b6bd51d2864bbcd5105778c
Daniel Connelly <dhconnelly@gmail.com>
2023-09-05 22:48:34 +0200

    c: day23part1

---
+++ c/day23.c
@@ -0,0 +1,106 @@
+#include <assert.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "parse.h"
+#include "vm.h"
+
+#define NUM_VMS 50
+#define MAX_PACKETS 100
+
+typedef struct {
+    int64_t x, y;
+} packet;
+
+typedef struct {
+    vm vm;
+    packet q[MAX_PACKETS];
+    int qp;
+} netvm;
+
+void init_netvm(netvm* vm, int addr, int64_t data[], int data_len) {
+    load_vm(&vm->vm, data, data_len);
+    for (int i = 0; i < MAX_PACKETS; i++) {
+        vm->q[i].x = 0;
+        vm->q[i].y = 0;
+    }
+    vm->qp = -1;
+
+    run(&vm->vm);
+    assert(vm->vm.state == VM_INPUT);
+    vm->vm.input = addr;
+    run(&vm->vm);
+}
+
+int64_t part1(int64_t data[], int data_len) {
+    // initialize and prime the vms
+    netvm vms[NUM_VMS];
+    for (int i = 0; i < NUM_VMS; i++) init_netvm(&vms[i], i, data, data_len);
+
+    for (int iter = 0;; iter++) {
+        for (int i = 0; i < NUM_VMS; i++) {
+            switch (vms[i].vm.state) {
+                case VM_INPUT: {
+                    if (vms[i].qp < 0) {
+                        vms[i].vm.input = -1;
+                        run(&vms[i].vm);
+                    } else {
+                        packet packet = vms[i].q[vms[i].qp--];
+                        vms[i].vm.input = packet.x;
+                        run(&vms[i].vm);
+                        assert(vms[i].vm.state == VM_INPUT);
+                        vms[i].vm.input = packet.y;
+                        run(&vms[i].vm);
+                    }
+                    break;
+                }
+
+                case VM_OUTPUT: {
+                    int64_t addr = vms[i].vm.output;
+                    run(&vms[i].vm);
+                    assert(vms[i].vm.state == VM_OUTPUT);
+                    int64_t x = vms[i].vm.output;
+                    run(&vms[i].vm);
+                    assert(vms[i].vm.state == VM_OUTPUT);
+                    int64_t y = vms[i].vm.output;
+                    run(&vms[i].vm);
+
+                    if (addr == 255) return y;
+
+                    assert(++vms[addr].qp < MAX_PACKETS - 1);
+                    vms[addr].q[vms[addr].qp].x = x;
+                    vms[addr].q[vms[addr].qp].y = y;
+                    break;
+                }
+
+                default:
+                    assert(0);
+            }
+        }
+    }
+
+    return 0;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "usage: day23 file\n");
+        exit(1);
+    }
+
+    FILE* f = fopen(argv[1], "r");
+    if (f == NULL) {
+        perror("day23");
+        exit(1);
+    }
+
+    int64_t data[4096];
+    int len;
+    if ((len = parse_intcode(f, data, 4096)) < 0) {
+        perror("day23: failed to parse intcode");
+        exit(1);
+    }
+
+    printf("%ld\n", part1(data, len));
+}
